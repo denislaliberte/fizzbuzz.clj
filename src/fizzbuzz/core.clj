adfd7e53cfe45f19e05549b141d07b357e27f6b4
Denis Laliberté <denis@tp1.ca>
2014-12-29 16:59:26 -0500

    refactor and add doc string

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,10 +1,10 @@
 (ns fizzbuzz.core)
 
-(defn isMod [a b ]
+(defn isMod "Test if the module of the first parameter by the second return 0" [a b ]
   (= (mod a b) 0)
 )
 
-(defn fizzbuzz [a]
+(defn fizzbuzz "Calculate the fizz or buzz of a number" [a]
   (cond
     (isMod a 15) "FizzBuzz"
     (isMod a 5) "Buzz"
@@ -13,6 +13,6 @@
   )
 )
 
-(defn fizzbuzzlist [a]
-  (map fizzbuzz (range 1 (inc a)))
+(defn fizzbuzzlist "return a list of fizz buzz from 1 to the max number" [max]
+  (map fizzbuzz (range 1 (inc max)))
 )
